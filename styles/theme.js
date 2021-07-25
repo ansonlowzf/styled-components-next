9213f60b96a0121629341fba005dd59d85fe2f0e
Anson Low Z.F <zhanfaye@gmail.com>
2021-07-25 12:17:53 +0800

    add secondary color

--- styles/theme.js
+++ styles/theme.js
@@ -1,12 +1,18 @@
 export const theme = {
   colors: {
     primary: "hsl(212, 100%, 48%)",
+    secondary: "hsl(46, 82%, 53%)",
   },
   bgColors: {
     primary: "hsl(210, 11%, 15%)",
   },
   breakpoints: {
     sm: "600px",
+    md: "960px",
+    lg: "1280px",
+  },
+  devices: {
+    sm: "screen and (max-width: 600px)",
     md: "960px",
     lg: "1280px",
   },
