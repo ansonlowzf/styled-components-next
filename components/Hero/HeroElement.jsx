3f544b913dbae367a631efb662f6ddee0fca0dc9
Anson Low Z.F <zhanfaye@gmail.com>
2021-07-25 12:18:11 +0800

    remove unused style

--- components/Hero/HeroElement.jsx
+++ components/Hero/HeroElement.jsx
@@ -16,26 +16,24 @@ export const HeroOverlay = styled.div`
   position: absolute;
   inset: 0;
   background-color: hsla(0, 0%, 0%, 0.3);
-
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index: 9;
 `;
 
 export const HeroContent = styled.div`
   width: min(70%, ${(props) => props.theme.breakpoints.lg});
-  z-index: 10;
 
-  @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
+  @media ${(props) => props.theme.devices.sm} {
     text-align: center;
   }
 `;
 
 export const HeroTitle = styled(Heading1)`
-  margin-bottom: 2rem;
+  margin-bottom: 3rem;
+  padding-bottom: 1rem;
   font-weight: bolder;
-  box-shadow: 3px 5px #e9ba23;
+  border-bottom: 5px solid ${(props) => props.theme.colors.secondary};
 
   @media screen and (max-width: ${(props) => props.theme.breakpoints.sm}) {
     text-align: center;
